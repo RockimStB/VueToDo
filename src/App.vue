3c6e10fb3f7749aec5a13bade3e0a85033787ad5
Rockim St. Bernard <rockim313@hotmail.com>
2020-06-01 12:13:56 -0400

    AddTodo page

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app" >
     <link rel="stylesheet" href="/CSS/tailwind.css"> <!--Using this link allows tailwind css to be used simular to linking a css style sheet-->
   <div class=container>
+    <AddTodo/>
     <Todos/>
     </div>
   </div>
@@ -9,11 +10,13 @@
 
 <script>
 import Todos from './components/Todos.vue'
+import AddTodo from './components/AddTodo.vue'
 
 export default {
   name: 'App',
   components: {
-    Todos
+    Todos,
+    AddTodo
   }
 }
 </script>
@@ -32,4 +35,22 @@ body {
   padding: 0 2rem;
 }
 
+form{
+  display:flex;
+}
+
+input[type="text"]{
+  flex:10;
+  padding: 10px;
+  border: 1px solid #41b883;
+  outline:0;
+}
+
+input[type= "submit"]{
+  flex:2;
+  background:#41b883;
+  color:#41b883;
+  border: 1px #41b883 solid ;
+  cursor: pointer;
+}
 </style>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1> Add Todo</h1>
+        <div class="add">
+            <form>
+                <input type="text" v-model= "title" placeholder="Add Todo">
+                <input type="submit" value= "submit">
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"AddTodo"
+}
+</script>
+
+<style scoped>
+
+</style>
